88e12248ec41e2c3c073862ad6f232381f378a49
ajitborude <tech.ajitb@gmail.com>
2025-04-16 00:15:45 +0530

    Added theme switch for guest user

--- src/layouts/guest.tsx
+++ src/layouts/guest.tsx
@@ -1,11 +1,15 @@
 import { GlobalSpinner } from '@/components/common/spinner';
+import { ThemeSwitch } from '@/components/common/theme-switch';
 import { Outlet, useNavigation } from 'react-router';
 
 export default function GuestLayout() {
   const navigation = useNavigation();
   const isNavigating = Boolean(navigation.location);
   return (
-    <div className="flex h-svh w-full justify-center items-center p-4">
+    <div className="flex h-svh w-full justify-center items-center p-4 relative">
+      <div className="absolute bottom-4 right-4">
+        <ThemeSwitch />
+      </div>
       {isNavigating && <GlobalSpinner />}
       <Outlet />
     </div>
